c752a40820ed90e89b25866e97cfc8648938acf6
lim web <limweb@hotmail.com>
2021-02-18 17:38:38 +0700

    firstcommit

---
+++ app/index.php
@@ -0,0 +1,2 @@
+<?php
+  echo "LiveView is a new game-changing library that gives you wings, drastically simplifying and speeding up the development of interactive, real-time Phoenix web applications.";
